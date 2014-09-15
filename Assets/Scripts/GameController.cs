5489c80f093117eca4b7c296545de555e4be0d01
wei <weiguxp@gmail.com>
2014-09-15 18:42:22 +0800

    BasicBlocks

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 	public  enum GameState
@@ -13,6 +14,10 @@ public class GameController : MonoBehaviour {
 
 	public static GameController CS;
 	public GameState gameState;
+
+	public GameObject WhiteSquare;
+	public GameObject BlackSquare;
+	private GameObject SquareObject;
 
 	//Settings for the game
 	private int numCols;
@@ -48,12 +53,14 @@ public class GameController : MonoBehaviour {
 		int[] blackSquareList = new int[num_blacks];
 		int[,] memoryMatrix  = new int[numCols,numRows];
 
+
+//		Consider using as hashset here
 		for (int j = 0; j <num_blacks; j++) {
 			int isNewNum = 0;
 			int i = -1;
-
+//		Prevent number duplication
 			while(isNewNum == 0){
-				i = Random.Range (0, numSquares);
+				i = Random.Range (0, numSquares-1);
 				isNewNum =1;
 				foreach (int k in blackSquareList){
 					if (k == i){ isNewNum=0;}
@@ -63,9 +70,32 @@ public class GameController : MonoBehaviour {
 			blackSquareList [j] = i;
 		}
 
-//		foreach (int i in blackSquareList){
-//			Debug.Log (i);
-//		}
+		foreach (int i in blackSquareList){
+			int x = i / numRows;
+			int y = i % numRows;
+			memoryMatrix[x,y] = 1;
+//			Debug.Log ("i = " + i);
+//			Debug.Log ( x + " , " + y);
+		}
 
+		for (int i = 0; i <numCols; i++){
+			for (int j = 0; j<numRows; j++){
+				if (memoryMatrix[i,j] == 1){
+					SquareObject = (GameObject)Instantiate(BlackSquare, new Vector3(i,j,0), Quaternion.identity);
+//					SquareObject.tag = "BlackSquare";
+				}
+			}
+		}
+
+		for (int i = 0; i <numCols; i++){
+			for (int j = 0; j<numRows; j++){
+				if (memoryMatrix[i,j] == 0){
+					SquareObject = (GameObject)Instantiate(WhiteSquare, new Vector3(i,j,0), Quaternion.identity);
+//					SquareObject.tag = "WhiteSquare";
+				}
+			}
+		}
+
+		
 	}
 }
