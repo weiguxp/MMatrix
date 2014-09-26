802e3d459c0dec75c34fc8aa5151014bf6419b6f
wei <weiguxp@gmail.com>
2014-09-26 18:38:19 +0800

    friday

---
+++ Assets/Scripts/Serialization/ISerializer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace com.phobos {
+	public interface ISerializer {
+		byte[] Serialize<T>(T obj);
+		T Deserialize<T>(byte[] bytes);
+		T Deserialize<T>(byte[] bytes, int startIndex, int count);
+	}
+	
+}
---
+++ Assets/Scripts/Serialization/ProtobufSerializer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+using ProtoBuf;
+
+namespace com.phobos {
+
+	public class ProtobufSerializer : ISerializer {
+		public byte[] Serialize<T>(T obj) {
+			using (MemoryStream ms = new MemoryStream()) {
+				Serializer.Serialize(ms, obj);
+				return ms.ToArray();
+			}
+		}
+
+		public T Deserialize<T>(byte[] bytes) {
+			int bytesLength = bytes.Length;
+			return Deserialize<T> (bytes, 0, bytesLength);
+		}
+
+		public T Deserialize<T>(byte[] bytes, int startIndex, int count) {
+			using (MemoryStream ms = new MemoryStream(bytes, startIndex, count)) {
+				return Serializer.Deserialize<T>(ms);
+			}
+		}
+
+	}
+	
+}
